bd4ec7d014df48af40e8ce7cf99f49fd5a7e7914
NOURISH CHERISH <nourishnew@gmail.com>
2019-10-04 11:43:49 -0400

    Add files via upload

---
+++ BaseViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Imhere.UWP
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void Set<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (Equals(field, value)) return;
+            field = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ MainViewModel.cs
@@ -0,0 +1,64 @@
+﻿using Imhere.UWP;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+
+namespace ImHere
+{
+    public class MainViewModel : BaseViewModel
+    {
+        string message = "";
+        public string Message
+        {
+            get => message;
+            set => Set(ref message, value);
+        }
+
+        string phoneNumbers = "";
+        public string PhoneNumbers
+        {
+            get => phoneNumbers;
+            set => Set(ref phoneNumbers, value);
+        }
+
+        public MainViewModel()
+        {
+            SendLocationCommand = new Command(async () => await SendLocation());
+        }
+
+
+        public ICommand SendLocationCommand { get; }
+        HttpClient client = new HttpClient();
+        const string baseUrl = "https://imherefunctions20191003113917.azurewebsites.net";
+
+        async Task SendLocation()
+        {
+            Location location = await Geolocation.GetLastKnownLocationAsync();
+
+            if (location != null)
+            {
+                Message = $"Location found: {location.Latitude}, {location.Longitude}.";
+                Imhere.Data.PostData postData = new Imhere.Data.PostData
+                {
+                    Latitude = location.Latitude,
+                    Longitude = location.Longitude,
+                    ToNumbers = PhoneNumbers.Split('\n')
+                };
+
+                string data = JsonConvert.SerializeObject(postData);
+                StringContent content = new StringContent(data, System.Text.Encoding.UTF8, "application/json");
+                HttpResponseMessage result = await client.PostAsync($"{baseUrl}/api/SendLocation",
+                                                                    content);
+
+                if (result.IsSuccessStatusCode)
+                    Message = "Location sent successfully";
+                else
+                    Message = $"Error - {result.ReasonPhrase}";
+            }
+        }
+    }
+}
